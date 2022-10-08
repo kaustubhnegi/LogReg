36caa9622e412baab6ee91cfb10388b879d2b189
kaustubhnegi <kaustubhnegi1@gmail.com>
2022-10-09 00:05:53 +0530

    ok

---
+++ node-js-express-login-mongodb/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import cookieSession from 'cookie-session';
+
+
+const app = express();
+
+var corsOption ={
+    origin : "http://localhost:8081"
+};
+
+const asm =10;
+// CORS is a node.js package for providing a Connect/Express middleware 
+// that can be used to enable CORS with various options.
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
